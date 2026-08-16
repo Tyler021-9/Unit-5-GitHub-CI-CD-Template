4d585f52be11216843a615c9ef71e1d9cd77f173
Tyler0219 <tjensen@herzing.edu>
2026-08-16 11:51:11 -0500

    Fix flake8 formatting issues

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -6,9 +7,11 @@ app = Flask(__name__)
 def home():
     return "Pipeline deployment successful!"
 
+
 @app.route("/status")
 def status():
     return "OK"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
